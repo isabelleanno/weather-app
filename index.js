c37ae2375e3d6051d33dd88a4dcc1f58b5055722
isabelleanno <79269204+isabelleanno@users.noreply.github.com>
2021-05-14 23:56:57 -0400

    Integrated weather icons, fixed bugs

--- index.js
+++ index.js
@@ -36,13 +36,51 @@ function showTemperature(response) {
   let feelsLike = response.data.main.feels_like;
   feelsLike = Math.round(feelsLike);
   let feels = document.querySelector("#feelsLike");
-  feels.innerHTML = `Feels like ${feelsLike}°C`;
+  feels.innerHTML = `Feels like: ${feelsLike}°C`;
 
   let humidity = response.data.main.humidity;
   humidity = Math.round(humidity);
   let humid = document.querySelector("#humid");
-  humid.innerHTML = `Humidity: ${humidity}`;
+  humid.innerHTML = `Humidity: ${humidity}%`;
+
+  let getDescription = response.data.weather[0].description;
+  let description = document.querySelector("#description");
+  description.innerHTML = `${getDescription}`;
+
+  //I wanted to integrate font awesome icons instead of the weather api icons, so this is the section that does that.
+
+  let weatherIcon = response.data.weather[0].icon;
+  console.log(weatherIcon);
+  let currentWeather = response.data.weather[0].main;
+  console.log(currentWeather);
+  let icon = document.querySelector("#icon");
+  if (currentWeather === "Clear") {
+    icon.innerHTML = ` <i class="fas fa-sun"></i>`;
+  } else {
+    if (currentWeather === "Clouds") {
+      icon.innerHTML = `<i class="fas fa-cloud"></i>`;
+    } else {
+      if (currentWeather === "Thunderstorm") {
+        icon.innerHTML = `<i class="fas fa-bolt"></i>`;
+      } else {
+        if (currentWeather === "Rain" || "Drizzle") {
+          icon.innerHTML = `<i class="fas fa-cloud-showers-heavy"></i>`;
+        } else {
+          if (currentWeather === "Snow") {
+            icon.innerHTML = `<i class="far fa-snowflake"></i>`;
+          } else {
+            if (weatherIcon === "02d" || "02n")
+              icon.innerHTML = `<i class="fas fa-cloud-sun"></i>`;
+            else {
+              icon.innerHTML = <i class="fas fa-smog"></i>;
+            }
+          }
+        }
+      }
+    }
+  }
 }
+
 //cityAlert displays the city you searched as well as generates an api key to send to the showTemperature function
 function cityAlert(event) {
   event.preventDefault();
@@ -58,15 +96,6 @@ function cityAlert(event) {
 let form = document.querySelector("#cityName");
 form.addEventListener("submit", cityAlert);
 
-//degres converts fahrenheit to celcius and vice versa
-function degree(event) {
-  if (button.className === "celsius") button.innerHTML = `C`;
-  button.className = "fahrenheit";
-}
-
-let button = document.querySelector("#degreeButton");
-button.addEventListener("click", degree);
-
 //hourChange converts 24 hour format to am/pm
 function hourChange(event) {
   if (time === 24) {
@@ -79,7 +108,7 @@ function hourChange(event) {
     )}`;
     time = 24;
   }
-  let time = 24;
-  let clock = document.querySelector("#time");
-  clock.addEventListener("click", hourChange);
 }
+let time = 24;
+let clock = document.querySelector("#time");
+clock.addEventListener("click", hourChange);
