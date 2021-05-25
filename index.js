cd56cf5d2a012fb8aa389b14dc58cd457032bc91
isabelleanno <79269204+isabelleanno@users.noreply.github.com>
2021-05-25 18:44:50 -0400

    Got weathericon to work for both the current weather and the forecast.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 //This is a lot of JavaScript. I will walk you through what everything does with these comments. First, we're going to start with general
 //stuff like the dates and times. Since JavaScript already knows this based on your location, we do not need an API. There are two API's used
 //in this weather app. One is for the current weather, and one is for the forecast.
-
 //Gets the current dates and times
-
 let now = new Date();
 let days = [
   "Sunday",
@@ -17,7 +15,6 @@ let days = [
 let day = days[now.getDay()];
 let currentDay = document.querySelector("#day");
 currentDay.innerHTML = `It is ${day},`;
-
 //hourChange converts 24 hour format to am/pm
 function hourChange(event) {
   if (time === 24) {
@@ -34,7 +31,6 @@ function hourChange(event) {
 let time = 24;
 let clock = document.querySelector("#time");
 clock.addEventListener("click", hourChange);
-
 let hour = [now.getHours()];
 let minute = (now.getMinutes() < 10 ? "0" : "") + now.getMinutes();
 let month = [now.getMonth() + 1];
@@ -44,14 +40,11 @@ let currentDate = document.querySelector("#date");
 currentDate.innerHTML = `<i class="far fa-calendar-alt"></i> ${month}/${date}/${year}`;
 let currentTime = document.querySelector("#time");
 currentTime.innerHTML = `<i class="far fa-clock"></i>  ${hour}:${minute}`;
-
 //Now we're getting into the first of two API's. This one deals with the current forecast. We start with a function called "convert" which does
 // two important things: 1.) It generates the API url. and 2.) It is able to convert the units from fahrenheit to celsius when you click
 //the "F" or "C" displayed on the screen. This function also converts the units for the forecast api, too using the same method,
 //since "units" is a global variable, so I just grabbed it and used it twice.
-
 let units = "";
-
 function convert(event) {
   event.preventDefault();
   let place = document.querySelector("#place");
@@ -65,7 +58,6 @@ function convert(event) {
     degreeButton.innerHTML = "C";
     units = "metric";
   }
-
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=dde5f5b53a9878edbb3c8ca7477531b0`;
   axios
     .get(apiUrl)
@@ -76,39 +68,34 @@ function convert(event) {
 }
 let degreeButton = document.querySelector("#degreeButton");
 degreeButton.addEventListener("click", convert);
-
 let form = document.querySelector("#cityName");
 form.addEventListener("submit", convert);
-
 //showTemperature gets the api from function convert and integrates it in the display.
-
 function showTemperature(response) {
   let temperature = response.data.main.temp;
   temperature = Math.round(temperature);
   let temp = document.querySelector("#currentTemp");
   temp.innerHTML = `${temperature}°`;
-
   let feelsLike = response.data.main.feels_like;
   feelsLike = Math.round(feelsLike);
   let feels = document.querySelector("#feelsLike");
   feels.innerHTML = `Feels like: ${feelsLike}°`;
-
   let humidity = response.data.main.humidity;
   humidity = Math.round(humidity);
   let humid = document.querySelector("#humid");
   humid.innerHTML = `Humidity: ${humidity}%`;
-
   let getDescription = response.data.weather[0].description;
   let description = document.querySelector("#description");
   description.innerHTML = `${getDescription}`;
-
   getForecast(response.data.coord);
-  weatherIcons(response);
+  let icon = document.querySelector(".icon");
+  icon.innerHTML = weatherIcons(
+    response.data.weather[0].icon,
+    response.data.weather[0].main
+  );
 }
-
 //Now we're moving onto the forecast/future  portion of this app. We start with function "getForecast" (very original name) which generates
 //the API and dstributes it to another function to display it.
-
 function getForecast(coordinates) {
   let lat = coordinates.lat;
   let lon = coordinates.lon;
@@ -116,14 +103,12 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(forecast);
 }
-
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
   let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
   return days[day];
 }
-
 //forecast displays the innerHTML of our forecast.
 function forecast(response) {
   let forecastDays = response.data.daily;
@@ -139,6 +124,7 @@ function forecast(response) {
         <div class="card-body">
           <h6>${formatDay(day.dt)}</h6>
           <p class="future-temp" class="icon">
+          ${weatherIcons(day.weather[0].icon, day.weather[0].main)}
           ${Math.round(day.temp.day)}°
           </p>
           <span>High: ${Math.round(day.temp.max)}°</span>
@@ -150,49 +136,32 @@ function forecast(response) {
       `;
     }
   });
-
   weatherForecast = weatherForecast + `</div>`;
   future.innerHTML = weatherForecast;
 }
-
 //weatherIcons generates the weather icon based on the description given by the API using a big ass if/else statement. I could have just used
 //the icons given to me by the API, but I think fontAwesome's icons are cuter and go better with this app. This function is used by both the
 //current weather stuff and by the forecast stuff.
-
-let icon = document.querySelector(".icon");
-
-function weatherIcons(response) {
-  let weatherIcon = response.data.weather[0].icon;
-  let currentWeather = response.data.weather[0].main;
+function weatherIcons(weatherIcon, currentWeather) {
+  let icon = "";
   if (weatherIcon === "01d") {
-    icon.innerHTML = ` <i class="fas fa-sun"></i>`;
+    icon = ` <i class="fas fa-sun"></i>`;
+  } else if (weatherIcon === "01n") {
+    icon = ` <i class="fas fa-moon"></i>`;
+  } else if (currentWeather === "Clouds") {
+    icon = `<i class="fas fa-cloud"></i>`;
+  } else if (currentWeather === "Thunderstorm") {
+    icon = `<i class="fas fa-bolt"></i>`;
+  } else if (currentWeather === "Rain" || "Drizzle") {
+    icon = `<i class="fas fa-cloud-showers-heavy"></i>`;
+  } else if (currentWeather === "Snow") {
+    icon = `<i class="far fa-snowflake"></i>`;
+  } else if (weatherIcon === "02d") {
+    icon = `<i class="fas fa-cloud-sun"></i>`;
+  } else if (weatherIcon === "02n") {
+    icon = `<i class="fas fa-cloud-moon"></i>`;
   } else {
-    if (weatherIcon === "01n") {
-      icon.innerHTML = ` <i class="fas fa-moon"></i>`;
-    } else if (currentWeather === "Clouds") {
-      icon.innerHTML = `<i class="fas fa-cloud"></i>`;
-    } else {
-      if (currentWeather === "Thunderstorm") {
-        icon.innerHTML = `<i class="fas fa-bolt"></i>`;
-      } else {
-        if (currentWeather === "Rain" || "Drizzle") {
-          icon.innerHTML = `<i class="fas fa-cloud-showers-heavy"></i>`;
-        } else {
-          if (currentWeather === "Snow") {
-            icon.innerHTML = `<i class="far fa-snowflake"></i>`;
-          } else {
-            if (weatherIcon === "02d")
-              icon.innerHTML = `<i class="fas fa-cloud-sun"></i>`;
-            else {
-              if (weatherIcon === "02n")
-                icon.innerHTML = `<i class="fas fa-cloud-moon"></i>`;
-              else {
-                icon.innerHTML = `<i class="fas fa-smog"></i>`;
-              }
-            }
-          }
-        }
-      }
-    }
+    icon = `<i class="fas fa-smog"></i>`;
   }
+  return icon;
 }
